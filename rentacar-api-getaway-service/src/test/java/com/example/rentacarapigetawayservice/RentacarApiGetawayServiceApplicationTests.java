138408c02e682bd4f870ad2cf70a4ece0fc00008
ljubicics <119794666+ljubicics@users.noreply.github.com>
2023-07-01 01:17:45 +0200

    Add files via upload

---
+++ rentacar-api-getaway-service/src/test/java/com/example/rentacarapigetawayservice/RentacarApiGetawayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rentacarapigetawayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentacarApiGetawayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
