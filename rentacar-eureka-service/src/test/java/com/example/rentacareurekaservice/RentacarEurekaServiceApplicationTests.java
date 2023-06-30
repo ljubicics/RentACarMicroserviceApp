2abc44d1919f1ba326c0ee3eb02c775ffe9df41e
ljubicics <119794666+ljubicics@users.noreply.github.com>
2023-07-01 01:18:03 +0200

    Add files via upload

---
+++ rentacar-eureka-service/src/test/java/com/example/rentacareurekaservice/RentacarEurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rentacareurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentacarEurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
